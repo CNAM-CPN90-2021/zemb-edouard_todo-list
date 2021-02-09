5cc4312150a86ba0a3a6a3cb872bc75cd5978038
Edouard Zemb <edouard.zemb@gmail.com>
2021-02-09 11:06:29 +0100

    ajout d'un style pour les item à l'état 'fait'

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,26 +1,30 @@
-import React, { useState } from 'react';
-import {
-  IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonInput,
-  IonTabBar, IonFooter, IonButton, IonIcon, IonLabel, IonCheckbox
-} from '@ionic/react';
+import React, { useState } from "react";
+import { IonItem, IonLabel, IonCheckbox } from "@ionic/react";
+import "./TodoListItem.css";
 
 interface ContainerProps {
   todoItem: string | undefined;
 }
 
 const TodoListItem: React.FC<ContainerProps> = ({ todoItem }) => {
-
   const [checked, setChecked] = useState(false);
+  const [className, setClassName] = useState("");
 
   return (
     <IonItem>
-      <IonLabel>{todoItem}</IonLabel>
-      <IonCheckbox 
-      checked={checked}
-      onIonChange={e => { setChecked(e.detail.checked); }}
-      slot="start" />
+      <IonLabel className={className}>{todoItem}</IonLabel>
+      <IonCheckbox
+        checked={checked}
+        onIonChange={(e) => {
+          const checkedValue = e.detail.checked
+          setChecked(checkedValue);
+          setClassName(checkedValue ? "item-done" : "");
+          console.log(className);
+        }}
+        slot="start"
+      />
     </IonItem>
-  )
+  );
 };
 
 export default TodoListItem;
